c029aead6ffe4ea4bb028018574ee7d1fe1a5064
Remi <urflamality@gmail.com>
2025-02-03 17:09:30 -0500

    Fit footer into contact component

--- src/pages/tabs/components/Footer.jsx
+++ src/pages/tabs/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className='w-full flex flex-row flex-wrap justify-center items-center p-2 gap-2'>
+    <div className='w-full bg-zinc-950 flex flex-row flex-wrap justify-center items-center p-2 pt-4 gap-2'>
       <p className='text-sm text-neutral-400'>
         Copyright 2025 Flamality. All rights reserved.
       </p>
