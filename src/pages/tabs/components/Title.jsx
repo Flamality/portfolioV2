8cb33e292d630edc0fa0224658d8d2c9160050c6
Remi <urflamality@gmail.com>
2025-02-26 21:28:41 -0500

    Fixed title bug

--- src/pages/tabs/components/Title.jsx
+++ src/pages/tabs/components/Title.jsx
@@ -2,36 +2,53 @@ import React, { useState, useEffect } from "react";
 
 const Title = ({
   sequence,
-  typingSpeed = 50,
+  typingSpeed = 80,
   pauseDuration = 3000,
   isChanging,
 }) => {
   const [displayText, setDisplayText] = useState("");
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [loopNum, setLoopNum] = useState(0);
+  const [isWaiting, setIsWaiting] = useState(false);
 
   useEffect(() => {
+    if (isWaiting) return;
+
+    const currentString = sequence[loopNum % sequence.length];
+
     const handleTyping = () => {
-      const currentString = sequence[loopNum % sequence.length];
-      const updatedText = isDeleting
-        ? currentString.substring(0, displayText.length - 1)
-        : currentString.substring(0, displayText.length + 1);
-
-      setDisplayText(updatedText);
-
-      if (!isDeleting && updatedText === currentString) {
-        setTimeout(() => setIsDeleting(true), pauseDuration);
-      } else if (isDeleting && updatedText === "") {
-        setIsDeleting(false);
-        setLoopNum(loopNum + 1);
+      if (isDeleting) {
+        setDisplayText((prevText) =>
+          prevText.substring(0, prevText.length - 1)
+        );
+        if (displayText === "") {
+          setIsDeleting(false);
+          setLoopNum((prevLoop) => prevLoop + 1);
+        }
+      } else {
+        const updatedText = currentString.substring(0, displayText.length + 1);
+        setDisplayText(updatedText);
+        if (updatedText === currentString) {
+          setIsWaiting(true);
+          setTimeout(() => {
+            setIsWaiting(false);
+            setIsDeleting(true);
+          }, pauseDuration);
+        }
       }
     };
 
     const typingTimer = setTimeout(handleTyping, typingSpeed);
-
     return () => clearTimeout(typingTimer);
-  }, [displayText, isDeleting, loopNum, sequence, typingSpeed, pauseDuration]);
+  }, [
+    displayText,
+    isDeleting,
+    loopNum,
+    sequence,
+    typingSpeed,
+    pauseDuration,
+    isWaiting,
+  ]);
 
   return (
     <span
